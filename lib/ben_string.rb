6b3780864c1db08a5d83698b09dbf1046db44a0e
Ben Bleything <ben@bleything.net>
2010-11-16 19:54:18 -0800

    add BenString#to_bool

--- lib/ben_string.rb
+++ lib/ben_string.rb
@@ -1,3 +1,7 @@
-class BenString
-  VERSION = '0.0.1'
+class BenString < String
+  VERSION = '1.0.0'
+
+  def to_bool
+    !!self
+  end
 end
--- test/test_ben_string.rb
+++ test/test_ben_string.rb
@@ -2,7 +2,8 @@ require "test/unit"
 require "ben_string"
 
 class TestBenString < Test::Unit::TestCase
-  def test_sanity
-    flunk "write tests or I will kneecap you"
+  def test_to_bool
+    str = BenString.new
+    assert str.to_bool
   end
 end
