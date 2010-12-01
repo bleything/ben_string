0e79645aef9cfd0a638f3dd6b69553852a5bbe19
Aaron Patterson <aaron.patterson@gmail.com>
2010-12-01 13:15:01 -0800

    fisting whitespace

--- lib/ben_string.rb
+++ lib/ben_string.rb
@@ -1,29 +1,32 @@
 class BenString < String
   module Version
     # FIXME: Refactor this to be read from a YAML file
-
     MAJOR = 1
     MINOR = 1
-    BUGFX = 0
-
+    BUGFX = 0  
     def self.version(sep = ".")
       return [MAJOR, MINOR, BUGFX].join(sep)
     end
-
-
     VERSION = ::BenString::Version.version
   end
-
   VERSION = ::BenString::Version::VERSION
-
+ 
   public
        
     def to_bool()
       return !!self
     end
-
     def to_ben_s()     
       return self
     end
-
 end
+
+
+
+
+
+
+    
+
+
+
