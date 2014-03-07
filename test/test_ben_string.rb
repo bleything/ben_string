be6d7cbbf04c16433431ff2f185bc6290c10e865
Davy Stevenson <davy.stevenson@gmail.com>
2014-03-06 16:32:38 -0800

    Let's see if changing to be order dependent fixes the tests

--- test/test_ben_string.rb
+++ test/test_ben_string.rb
@@ -2,6 +2,13 @@ require "minitest/autorun"
 require "ben_string"
 
 class TestBenString < Minitest::Test
+  i_suck_and_my_tests_are_order_dependent!
+
+  def test_0_not_in_use
+    GC.start
+    refute BenString.in_use?
+  end
+
   def test_to_bool
     str = BenString.new
     assert str.to_bool
@@ -12,10 +19,6 @@ class TestBenString < Minitest::Test
     assert_equal str, str.to_ben_s
   end
 
-  def test_not_in_use
-    GC.start
-    assert !BenString.in_use?
-  end
 
   def test_in_use
     BenString.new
